ad261391ff9d40215bcf2b289d8885ba51ce324a
Daniel Rodriguez <127802925+danielrodc@users.noreply.github.com>
2023-11-25 01:43:05 +0000

    delete all button

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,47 +1,57 @@
 import React, { useState } from "react";
 
-//create your first component
 const Home = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
 
-	const [inputValue, setInputValue] = useState("");
-	const [todos, setTodos] = useState([]);
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
 
-	
-	return (
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      setTodos(todos.concat([inputValue]));
+      setInputValue("");
+    }
+  };
 
-		<div className="container-flex">
-			<h1>My To do List</h1>
-			<div className="container">
-			<ul>
-				<li>
-					<input
-						type="text"
-						onChange={(e) => setInputValue(e.target.value)}
-						value={inputValue}
-						onKeyPress={(e) => {
-							if (e.key === "Enter") {
-								setTodos(todos.concat([inputValue]));
-								setInputValue("");
-							}
-						}}
-						placeholder="Add your task here">
-					</input>
-				</li>
-				{todos.map((_item, index) => (
-					<li key={index}>
-						<span>{_item}</span>
-						<i
-							className="fas fa-trash-alt"
-							onClick={() => setTodos(
-								todos.filter(
-									(t, currentIndex) => index != currentIndex
-								)
-							)} ></i>
-					</li>))}
-			</ul>
-			<div>  Tasks: {todos.length}</div>
-			</div>
-		</div>
-	)
-								}
-	export default Home;
+  const handleDeleteTodo = (index) => {
+    setTodos(todos.filter((t, currentIndex) => index !== currentIndex));
+  };
+
+  const handleDeleteAll = () => {
+    setTodos([]);
+  };
+
+  return (
+    <div className="container-flex">
+      <h1>My To-Do List</h1>
+      <div className="container">
+        <ul>
+          <li>
+            <input
+              type="text"
+              onChange={handleChange}
+              value={inputValue}
+              onKeyPress={handleKeyPress}
+              placeholder="Add your task here"
+            />
+          </li>
+          {todos.map((todo, index) => (
+            <li key={index}>
+              <span>{todo}</span>
+              <i
+                className="fas fa-trash-alt"
+                onClick={() => handleDeleteTodo(index)}
+              ></i>
+            </li>
+          ))}
+        </ul>
+        <div>Tasks: {todos.length}</div>
+        <button onClick={handleDeleteAll}>Delete all</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
